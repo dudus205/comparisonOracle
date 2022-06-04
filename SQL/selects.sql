be2469cd3cf49110d21d5b8e759703879a7333be
Piotr Kaczmarczyk <kaczmarczyk9042@outlook.com>
2022-06-04 11:19:30 +0200

    Added new selects

--- SQL/selects.sql
+++ SQL/selects.sql
@@ -1,5 +1,3 @@
-
-
 -- Klienci z największą ilością wygranych aukcji
 -- Podzapytanie + COUNT
 select CUSTOMER.NAME, CUSTOMER.SURNAME, WonAuctions
@@ -10,10 +8,48 @@ FROM (select CUSTOMER.ID, COUNT(A2.WINNERID) as WonAuctions
       order by COUNT(A2.WINNERID) desc) s
          join CUSTOMER on CUSTOMER.ID = s.ID;
 
+
 -- Średnia ilość ofert na aukcję złożoną przez klienta
 -- Podzapytanie + AVG
 select CUSTOMERID, AVG(BID.BID)
 from BID
 group by CUSTOMERID;
 
+
+select * from auction;
+select * from customer;
+
+-- select pokazujacy ranking wg. najwiekszej liczby opini o pracowniku
+SELECT
+    em.id
+    ,em.name
+    ,em.surname
+    ,fb.cnt_emp
+    ,rank() over(order by cnt_emp) rank
+FROM
+    employee em
+JOIN (
+    SELECT
+        employeeid,
+        COUNT(employeeid) cnt_emp
+    FROM
+        feedback
+    group by
+        employeeid
+) fb ON fb.employeeid = em.id
+order by rank() over(order by cnt_emp) desc
+;
+
+--select z joinem na like
+SELECT
+    veh.manufacturer
+    ,veh.name
+    ,invi.grossvalue
+FROM
+    invoice_item invi
+JOIN vehicle veh ON
+    invi.vehicleid = veh.id
+    AND UPPER(veh.manufacturer) like '%IN%'
+order by invi.grossvalue desc
+;
 
