6eeeedea1a233de18a7bee89079d08fba8553d8e
Przemysław Postrch <przemyslaw.postrach@hotmail.com>
2022-06-11 17:56:38 +0200

    Add arbitrary invoice

--- Object/faktura.sql
+++ Object/faktura.sql
@@ -1,21 +1,26 @@
-CREATE OR REPLACE TYPE polskafaktura AS OBJECT
+drop table tabela_faktura;
+drop type polskafaktura;
+drop type niemieckafaktura;
+drop type faktura;
+
+CREATE OR REPLACE TYPE faktura AS OBJECT
 (
     DATA       DATE,
     KWOTANETTO DECIMAL,
-    CONSTRUCTOR FUNCTION POLSKAFAKTURA(DATA DATE) RETURN SELF AS RESULT,
-    CONSTRUCTOR FUNCTION POLSKAFAKTURA(DATA DATE, KWOTANETTO DECIMAL) RETURN SELF AS RESULT,
+    CONSTRUCTOR FUNCTION faktura(DATA DATE) RETURN SELF AS RESULT,
+    CONSTRUCTOR FUNCTION faktura(DATA DATE, KWOTANETTO DECIMAL) RETURN SELF AS RESULT,
     MEMBER FUNCTION STAWKAVAT RETURN INT,
     MEMBER FUNCTION WYLICZBRUTTO RETURN DECIMAL,
     MEMBER PROCEDURE DRUKUJ
-) instantiable not final;
+) not final;
 
-CREATE OR REPLACE TYPE BODY polskafaktura AS
-    CONSTRUCTOR FUNCTION POLSKAFAKTURA(DATA DATE) RETURN SELF AS RESULT IS
+CREATE OR REPLACE TYPE BODY faktura AS
+    CONSTRUCTOR FUNCTION faktura(DATA DATE) RETURN SELF AS RESULT IS
     BEGIN
         SELF.DATA := DATA;
         RETURN;
     END;
-    CONSTRUCTOR FUNCTION POLSKAFAKTURA(DATA DATE, KWOTANETTO DECIMAL) RETURN SELF AS RESULT IS
+    CONSTRUCTOR FUNCTION faktura(DATA DATE, KWOTANETTO DECIMAL) RETURN SELF AS RESULT IS
     BEGIN
         SELF.DATA := DATA;
         SELF.KWOTANETTO := KWOTANETTO;
@@ -23,29 +28,46 @@ CREATE OR REPLACE TYPE BODY polskafaktura AS
     END;
     MEMBER FUNCTION STAWKAVAT RETURN INT IS
     BEGIN
-        RETURN 23;
+        RETURN 1;
     END;
     MEMBER FUNCTION WYLICZBRUTTO RETURN DECIMAL IS
     BEGIN
-        RETURN KWOTANETTO;
+        RETURN KWOTANETTO * ((STAWKAVAT / 100) + 1);
     END;
     MEMBER PROCEDURE DRUKUJ IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('');
+    END;
+END;
+
+CREATE OR REPLACE TYPE polskafaktura UNDER faktura
+(
+    OVERRIDING MEMBER FUNCTION STAWKAVAT RETURN INT,
+    OVERRIDING MEMBER PROCEDURE DRUKUJ
+) instantiable final;
+
+CREATE OR REPLACE TYPE BODY polskafaktura AS
+    OVERRIDING MEMBER FUNCTION STAWKAVAT RETURN INT IS
+    BEGIN
+        RETURN 23;
+    END;
+    OVERRIDING MEMBER PROCEDURE DRUKUJ IS
     BEGIN
         DBMS_OUTPUT.PUT_LINE(SELF.DATA ||
                              ' Faktura PL ' ||
                              SELF.KWOTANETTO ||
-                             ' ' ||
-                             'Stawka VAT: ' ||
+                             ' Stawka VAT: ' ||
                              SELF.STAWKAVAT ||
+                             ' Kwota Brutto: ' ||
                              SELF.WYLICZBRUTTO);
     END;
 END;
 
-CREATE OR REPLACE TYPE niemieckafaktura UNDER polskafaktura
+CREATE OR REPLACE TYPE niemieckafaktura UNDER faktura
 (
     OVERRIDING MEMBER FUNCTION STAWKAVAT RETURN INT,
     OVERRIDING MEMBER PROCEDURE DRUKUJ
-) instantiable not final;
+) instantiable final;
 
 CREATE OR REPLACE TYPE BODY niemieckafaktura AS
     OVERRIDING MEMBER FUNCTION STAWKAVAT RETURN INT IS
@@ -54,8 +76,12 @@ CREATE OR REPLACE TYPE BODY niemieckafaktura AS
     END;
     OVERRIDING MEMBER PROCEDURE DRUKUJ IS
     BEGIN
-        DBMS_OUTPUT.PUT_LINE(SELF.DATA || ' Faktura DE ' || SELF.KWOTANETTO || ' ' || 'Stawka VAT: ' ||
+        DBMS_OUTPUT.PUT_LINE(SELF.DATA ||
+                             ' Faktura DE ' ||
+                             SELF.KWOTANETTO ||
+                             ' Stawka VAT: ' ||
                              SELF.STAWKAVAT ||
+                             ' Kwota Brutto: ' ||
                              SELF.WYLICZBRUTTO);
     END;
 END;
@@ -69,3 +95,15 @@ BEGIN
     f2 := niemieckafaktura(SYSDATE, 100);
     f2.DRUKUJ();
 END;
+
+CREATE TABLE tabela_faktura OF faktura;
+INSERT INTO tabela_faktura
+VALUES (polskafaktura(SYSDATE, 100));
+INSERT INTO tabela_faktura
+VALUES (niemieckafaktura(SYSDATE, 100));
+SELECT f.*, f.STAWKAVAT(), f.WYLICZBRUTTO()
+FROM tabela_faktura f;
+SELECT COUNT(*)
+FROM TABELA_FAKTURA;
+
+
